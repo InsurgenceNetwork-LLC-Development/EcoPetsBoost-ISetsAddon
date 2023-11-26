23cb30d190205bed88590c6d6594d76762e6a592
iHxtch <84823076+iHxtch@users.noreply.github.com>
2023-11-25 21:24:53 -0500

    init

---
+++ src/main/java/org/insurgencedev/ecopetsboost/EcoPetsBoostAddon.java
@@ -0,0 +1,20 @@
+package org.insurgencedev.ecopetsboost;
+
+import org.bukkit.Bukkit;
+import org.insurgencedev.insurgencesets.api.addon.ISetsAddon;
+import org.insurgencedev.insurgencesets.api.addon.InsurgenceSetsAddon;
+
+@ISetsAddon(name = "EcoPetsBoost", version = "1.0.0", author = "Insurgence Dev Team", description = "Boost the pet experience earned from EcoPets")
+public class EcoPetsBoostAddon extends InsurgenceSetsAddon {
+
+    @Override
+    public void onAddonStart() {
+        if (isDependentEnabled()) {
+            registerEvent(new PetExperienceGainListener());
+        }
+    }
+
+    private boolean isDependentEnabled() {
+        return Bukkit.getPluginManager().isPluginEnabled("EcoPets");
+    }
+}
